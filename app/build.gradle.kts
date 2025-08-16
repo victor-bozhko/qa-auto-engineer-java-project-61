595a5d262a5b1656bfbc44d076710b8967593fbb
victor <bozhko.v.n@yandex.ru>
2025-08-16 10:54:22 +0300

    fix build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,15 +16,6 @@ repositories {
     mavenCentral()
 }
 
-dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-}
-
-tasks.test {
-    useJUnitPlatform()
-}
-
 tasks.getByName("run", JavaExec::class) {
     standardInput = System.`in`
 }
